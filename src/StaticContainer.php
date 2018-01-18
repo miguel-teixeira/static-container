b901b2c7a6dca5515dd6760e14847f8576bdb99a
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2018-01-18 10:26:50 +0000

    add to git

---
+++ src/StaticContainer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace StaticContainer;
+
+trait StaticContainerTrait
+{
+    protected static $bindings = [];
+
+    public static function resolve(string $type)
+    {
+        $binding = static::bindings()[$type] ?? null;
+
+        if (is_callable($binding)) {
+            return $binding();
+        }
+
+        return $binding;
+    }
+
+    public static function bindings(): array
+    {
+        return static::$bindings;
+    }
+
+    public static function setBindings(array $bindings)
+    {
+        static::$bindings = $bindings;
+    }
+
+    public static function bindClass(string $key, string $class)
+    {
+        static::$bindings[$key] = $class;
+    }
+
+    public static function bindCallback(string $key, callable $callable)
+    {
+        static::$bindings[$key] = $callable;
+    }
+
+    public static function unbind(string $key)
+    {
+        unset(static::$bindings[$key]);
+    }
+}
