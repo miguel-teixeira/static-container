2f30d2be31fc6fe944e4f7a0626e8fcbae05231e
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2018-01-21 11:00:46 +0000

    add self factory

---
+++ src/StaticSelfFactoryTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace StaticContainer;
+
+trait StaticSelfFactoryTrait
+{
+    protected static $binding = null;
+
+    public static function binding()
+    {
+        return static::$binding;
+    }
+
+    public static function setBinding($binding)
+    {
+        static::$binding = $binding;
+    }
+
+    public static function resolve(...$args)
+    {
+        $binding = static::binding();
+
+        if (is_string($binding)) {
+            return new $binding(...$args);
+        }
+
+        if (is_callable($binding)) {
+            return $binding(...$args);
+        }
+
+        return new static(...$args);
+    }
+
+    public static function make(...$args)
+    {
+        return static::resolve(...$args);
+    }
+}
